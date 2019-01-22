205ffcff6955cb53cb09486e5b57464b7e4c8bf5
Jaap <jhpa.rodenburg@gmail.com>
2019-01-22 19:57:15 +0100

    Fix

--- WindowsServer/start.bat
+++ WindowsServer/start.bat
@@ -1,1 +1,21 @@
 BallRena.exe -batchmode -nographics
+
+
+FROM ubuntu:16.04
+
+WORKDIR /server
+
+COPY ./server
+
+RUN apt-get install wget -y && apt-get install zip -y 
+RUN wget https://ballrena.ml/build.zip
+RUN unzip build.zip
+RUN cd LinuxServer
+
+EXPOSE *
+
+CMD["./build/BallRenaLinux.x86_64 -batchmode -nographics"]
+
+
+
+ 
